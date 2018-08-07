6217b4b9e2d00ef06c722d5a50edcd637ef6853f
lance_Liu <lyf961336122@163.com>
2018-08-07 17:26:47 +0800

    lance

--- article/urls.py
+++ article/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('<int:my_args>', views.detail, name='detail'),
+    path('', views.home, name='home'),
+    path('<int:id>', views.detail, name='detail'),
     path('test/', views.test, name='test')
 
-    # 详情的意思
+
 ]
 
 
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from article import migrations
+
+from article.models import Article
 #admin lyf961336122
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('article.urls')),
-    # path('home/', include('article.urls')),
-    # path('detail/', include('article.urls')),
-    # path('test/', include('article.urls')),
+
+
 ]
